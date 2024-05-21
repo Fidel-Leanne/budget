ff7e5f111e05a93a124047c3eba0ac89df6a43fa
Fidelia M <fidelialtm13@gmail.com>
2024-05-21 04:01:28 +0200

    touchable without feedback

--- components/Sidebar.jsx
+++ components/Sidebar.jsx
@@ -1,39 +1,49 @@
 import React from 'react';
-import { View, Text, TouchableOpacity, SafeAreaView } from 'react-native';
+import { View, Text, TouchableOpacity, SafeAreaView, TouchableWithoutFeedback } from 'react-native';
 import { useRouter } from 'expo-router';
 import { MaterialIcons } from '@expo/vector-icons';
 
 const Sidebar = ({ isVisible, onClose }) => {
   const router = useRouter();
 
+  const handlePressOutside = () => {
+    if (isVisible) {
+      onClose();
+    }
+  };
+
   if (!isVisible) return null;
 
   return (
-    <SafeAreaView className="absolute top-0 left-0 h-[690px] w-[250px] bg-slate-800 bg-opacity-70 shadow-lg z-50 mt-6 rounded-3xl ">
+    <TouchableWithoutFeedback onPress={handlePressOutside}>
+         <SafeAreaView className="absolute top-0 left-0 h-[690px] w-[250px] bg-gray-900 bg-opacity-70 shadow-lg z-50 mt-6 rounded-3xl ">
       <View className="p-4">
         <TouchableOpacity onPress={onClose} className="mb-4">
           <MaterialIcons name="close" size={24} color="white" />
         </TouchableOpacity>
         <TouchableOpacity onPress={() => { router.push('/dashboard'); onClose(); }} className="mb-4">
-          <Text className="text-white text-lg">Dashboard</Text>
+          <Text className="text-white font-psemibold text-lg">Dashboard</Text>
         </TouchableOpacity>
         <TouchableOpacity onPress={() => { router.push('/accounts'); onClose(); }} className="mb-4">
-          <Text className="text-white text-lg">Accounts</Text>
+          <Text className="text-gray-300 font-psemibold text-lg">Accounts</Text>
         </TouchableOpacity>
         <TouchableOpacity onPress={() => { router.push('/transactions'); onClose(); }} className="mb-4">
-          <Text className="text-white text-lg">Transactions</Text>
+          <Text className="text-gray-300 font-psemibold  text-lg">Transactions</Text>
         </TouchableOpacity>
         <TouchableOpacity onPress={() => { router.push('/budgets'); onClose(); }} className="mb-4">
-          <Text className="text-white text-lg">Budgets</Text>
+          <Text className="text-gray-300 font-psemibold text-lg">Budgets</Text>
         </TouchableOpacity>
         <TouchableOpacity onPress={() => { router.push('/settings'); onClose(); }} className="mb-4">
-          <Text className="text-white text-lg">Settings</Text>
+          <Text className="text-gray-300 font-psemibold text-lg">Settings</Text>
         </TouchableOpacity>
-        <TouchableOpacity onPress={() => { /* Add logout functionality here */ onClose(); }} className="mt-4">
-          <Text className="text-white text-lg">Logout</Text>
+        <TouchableOpacity onPress={() => { /* Add logout functionality here */ onClose(); }} className="mt-[370px] bg-slate-700 rounded-xl">
+          <Text className="text-orange-500 text-lg text-center font-psemibold h-9 ">Logout</Text>
         </TouchableOpacity>
       </View>
     </SafeAreaView>
+
+    </TouchableWithoutFeedback>
+   
   );
 };
 
